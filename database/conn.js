d1c790a2f64d26e85d14d91710651b20958debd2
David oluoch <oluochdave175@gmail.com>
2023-02-01 15:43:19 +0300

    port adjustment

--- database/conn.js
+++ database/conn.js
@@ -1,3 +1,3 @@
 const {Sequelize} = require("sequelize")
-const sequelize = new Sequelize({username:process.env.DB_USERNAME , password:process.env.DB_PASSWORD  , database:process.env.DATABASE  , dialect:"mysql" , host:process.env.DB_HOST,host:process.env.DB_PORT})
+const sequelize = new Sequelize({username:process.env.DB_USERNAME , password:process.env.DB_PASSWORD  , database:process.env.DATABASE  , dialect:"mysql" , host:process.env.DB_HOST,port:process.env.DB_PORT})
 module.exports = sequelize
